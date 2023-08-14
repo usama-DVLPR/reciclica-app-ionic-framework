43a94a90ec6305954157821fb7eb24fb345b3c33
UsamaHameed02 <113657499+usama-DVLPR@users.noreply.github.com>
2023-08-14 08:23:01 +0500

    apply validation to LOGIN form

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginPageForm } from './login.page.form';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  constructor(private _router: Router) {}
+  form: FormGroup;
 
-  ngOnInit() {}
+  constructor(private _router: Router, private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = new LoginPageForm(this.formBuilder).createForm();
+  }
   login() {
     this._router.navigate(['home']);
   }
